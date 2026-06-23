7c0d8d230e91bff32d36ed2ac776ff75582c39b3
Sarawut Phaekuntod <sarawut@idevs.work>
2026-06-23 14:59:19 +0700

    feat: scaffold Themia.Modules.Identity.Tokens.AspNetCore package

---
+++ src/modules/Themia.Modules.Identity.Tokens.AspNetCore/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Themia.Modules.Identity.ExternalAuth.AspNetCore")]
+[assembly: InternalsVisibleTo("Themia.Modules.Identity.AspNetCore")]
+[assembly: InternalsVisibleTo("Themia.Modules.Identity.Tokens.AspNetCore.Tests")]
